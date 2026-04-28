415ddaa838f8ffaef07fdb54dd562b725e89934d
iamtoto <jackwj368@gmail.com>
2026-04-28 11:30:55 -0400

    basic generator

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ DRUM_NAMES = {
     37: "snare",
     38: "hat"
 }
+STEPS_PER_BAR = 16
+BEATS_PER_BAR = 4
 
 def load_template(path):
     mid = mido.MidiFile(path)
@@ -27,6 +29,26 @@ def load_template(path):
 
     return events
 
+def events_to_grid(events):
+    grid = {
+        "kick": [0] * STEPS_PER_BAR,
+        "snare": [0] * STEPS_PER_BAR,
+        "hat": [0] * STEPS_PER_BAR
+    }
+
+    for e in events:
+        beat_time = e["time"] % BEATS_PER_BAR
+
+        step = round(beat_time / BEATS_PER_BAR * STEPS_PER_BAR)
+
+        if step == STEPS_PER_BAR:
+            step = 0
+
+        drum = e["drum"]
+        grid[drum][step] = 1
+
+    return grid
+
 style = sys.argv[1] if len(sys.argv) > 1 else "lofi"
 
 if style not in ["lofi", "trap", "house"]:
@@ -35,9 +57,9 @@ if style not in ["lofi", "trap", "house"]:
 
 path = f"templates/{style}.mid"
 events = load_template(path)
+grid = events_to_grid(events)
 
-print(f"Loaded {style} template")
-print("First 10 events:")
+print(f"Loaded {style} template as grid:\n")
 
-for e in events[:10]:
-    print(e)
+for drum, pattern in grid.items():
+    print(drum.ljust(6), pattern)
