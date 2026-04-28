455509c16a89e5c016108d36ef0670421a5ac854
iamtoto <jackwj368@gmail.com>
2026-04-28 11:08:45 -0400

    changed midi files for ease and adjusted code

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import mido
+import sys
 
 DRUM_NAMES = {
     36: "kick",
-    38: "snare",
-    42: "hat"
+    37: "snare",
+    38: "hat"
 }
 
 def load_template(path):
@@ -16,7 +17,7 @@ def load_template(path):
         time_accum += msg.time
 
         if msg.type == "note_on" and msg.velocity > 0:
-            if msg.note in [36, 38, 42]:
+            if msg.note in DRUM_NAMES:
                 events.append({
                     "time": time_accum,
                     "note": msg.note,
@@ -26,8 +27,17 @@ def load_template(path):
 
     return events
 
-events = load_template("templates/lofi1.mid")
+style = sys.argv[1] if len(sys.argv) > 1 else "lofi"
 
+if style not in ["lofi", "trap", "house"]:
+    print("Invalid style. Choose: lofi, trap, or house")
+    exit()
+
+path = f"templates/{style}.mid"
+events = load_template(path)
+
+print(f"Loaded {style} template")
 print("First 10 events:")
+
 for e in events[:10]:
     print(e)
